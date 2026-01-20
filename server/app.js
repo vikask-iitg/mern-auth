e329465b93568028d31b40f5e9cab356e95ec39f
vikask-iitg <vikask.iitg@gmail.com>
2026-01-20 12:21:19 +0530

    file structure changed

---
+++ server/app.js
@@ -0,0 +1,52 @@
+// import express from 'express';
+// import cors from "cors";
+// import 'dotenv/config';
+// import cookieParser from 'cookie-parser'
+// import connectDB from './config/mongodb.js';
+// import authRouter from './routes/authRoutes.js';
+// import userRouter from './routes/userRoutes.js';
+
+// const app = express();
+// const port = process.env.PORT || 4000;
+// connectDB();
+
+// const allowedOrigins = ['http://localhost:5173']
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({origin: allowedOrigins, credentials: true }));
+
+// // API Endpoints
+// app.get('/', (req, res) => res.send("API is working!"));
+// app.use('/api/auth', authRouter)
+// app.use('/api/user', userRouter)
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+// export default app;
+
+
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).send("API Working");
+});
+
+export default app;
+
+
