393ade96856a6bb54f15478d0dce3847689a640c
vikask-iitg <vikask.iitg@gmail.com>
2026-01-20 17:00:42 +0530

    dropdown updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -46,87 +46,32 @@ const Navbar = () => {
     }
   };
 
-  // return (
-  //   <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 z-50">
-  //     <img src={assets.logo} alt="" className="w-28 sm:w-32" />
-  //     {userData ? (
-  //       <div
-  //         onClick={() => setOpen(!open)}
-  //         className="w-8 h-8 flex justify-center items-center rounded-full bg-black
-  //        text-white relative group"
-  //       >
-  //         {userData.name[0].toUpperCase()}
-  //         <div
-  //           className="absolute hidden group-hover:block top-0 right-0 z-10 text-black
-  //         rounded pt-10"
-  //         >
-  //           <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
-  //             {!userData.isAccountVerified && (
-  //               <li
-  //                 onClick={sendVerificationOtp}
-  //                 className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
-  //               >
-  //                 Verify Email
-  //               </li>
-  //             )}
-  //             <li
-  //               onClick={logout}
-  //               className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10"
-  //             >
-  //               Logout
-  //             </li>
-  //           </ul>
-  //         </div>
-  //       </div>
-  //     ) : (
-  //       <button
-  //         onClick={() => navigate("/login")}
-  //         className="flex items-center gap-2 border border-gray-500
-  //               rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer"
-  //       >
-  //         Login
-  //         <img src={assets.arrow_icon} alt="" />
-  //       </button>
-  //     )}
-  //   </div>
-  // );
-
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 z-50">
-      <img src={assets.logo} alt="logo" className="w-28 sm:w-32" />
-
+      <img src={assets.logo} alt="" className="w-28 sm:w-32" />
       {userData ? (
-        <div className="relative group">
-          {/* Avatar */}
+        <div
+          onClick={() => setOpen(!open)}
+          className="w-8 h-8 flex justify-center items-center rounded-full bg-black
+         text-white relative group"
+        >
+          {userData.name[0].toUpperCase()}
           <div
-            onClick={() => setOpen((prev) => !prev)}
-            className="w-8 h-8 flex justify-center items-center rounded-full
-            bg-black text-white cursor-pointer select-none"
+            className="absolute hidden group-hover:block top-0 right-0 z-10 text-black
+          rounded pt-10"
           >
-            {userData.name[0].toUpperCase()}
-          </div>
-
-          {/* Dropdown */}
-          <div
-            className={`
-              absolute top-0 right-0 z-10 pt-10
-              ${open ? "block" : "hidden"}
-              group-hover:block
-            `}
-          >
-            <ul className="list-none m-0 p-2 bg-gray-100 text-sm rounded shadow-md min-w-[140px]">
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
               {!userData.isAccountVerified && (
                 <li
                   onClick={sendVerificationOtp}
-                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded"
+                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
                 >
                   Verify Email
                 </li>
               )}
-
               <li
                 onClick={logout}
-                className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded"
+                className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10"
               >
                 Logout
               </li>
@@ -136,11 +81,11 @@ const Navbar = () => {
       ) : (
         <button
           onClick={() => navigate("/login")}
-          className="flex items-center gap-2 border border-gray-500
-          rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer"
+          className="flex items-center gap-2 border border-gray-500 
+                rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer"
         >
           Login
-          <img src={assets.arrow_icon} alt="arrow" />
+          <img src={assets.arrow_icon} alt="" />
         </button>
       )}
     </div>
