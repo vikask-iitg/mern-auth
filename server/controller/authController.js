bc6804326b30dffe679fe59a21b1b06603c98daa
vikask-iitg <vikask.iitg@gmail.com>
2026-01-22 14:35:58 +0530

    lax updated in login

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -77,7 +77,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
