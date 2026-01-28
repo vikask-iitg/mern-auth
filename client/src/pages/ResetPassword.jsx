90570da089c6f60218a5d1a68b81a71cdbee5e0d
vikask-iitg <vikask.iitg@gmail.com>
2026-01-28 13:10:01 +0530

    password reset page updated

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -150,10 +150,10 @@ const ResetPassword = () => {
         (<form onSubmit={onSubmitOtp}
           className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
           <h1 className="text-white text-2xl font-semibold text-center mb-4">
-            Reset password OTP
+            Reset Password OTP
           </h1>
           <p className="text-center mb-6 text-indigo-300">
-            Enter the 6-digit code sent to our email id.
+            Enter the 6-digit code sent to your email id.
           </p>
           <div className="flex justify-between mb-8" onPaste={handlePaste}>
             {Array(6)
